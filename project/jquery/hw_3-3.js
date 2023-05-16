49b9d55ac94ae6d69c4515ca590a80461dfc5030
sec3582 <131690941+sec3582@users.noreply.github.com>
2023-05-17 02:00:06 +0800

    version6

---
+++ project/jquery/hw_3-3.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+
+    $('.menu li').click(function(){
+        $(this).addClass('active-1').siblings().removeClass('active-1');
+    });
+
+    //main-1
+    $('.bugar-menu li a').click(function(){
+        $(this).addClass('active-2').parent().siblings().find('a').removeClass('active-2');
+    });
+    //main-3
+    $('.main-3 .search .pure-form button').click(function(){
+        $(this).toggleClass('active-3');
+        $('.main-3 .search .pure-form input').toggleClass('active-4');
+    });
+    $('.cart button').click(function(){
+        $(this).toggleClass('active-5');
+    });
+
+    //main-4
+    $(' .main-4 .goods button').click(function(){
+        $(this).toggleClass('active-6');
+        
+    });
+});
