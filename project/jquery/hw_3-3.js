9cf3cf8c512c50d75fee2474243a6b43bf6484bb
sec3582 <131690941+sec3582@users.noreply.github.com>
2023-05-29 05:03:37 +0800

    v19

--- project/jquery/hw_3-3.js
+++ project/jquery/hw_3-3.js
@@ -1,22 +1,28 @@
 $(document).ready(function(){
 
 
-    //main-1
+    //main-1-menu
     $('.bugar-menu li a').click(function(){
         $(this).addClass('active-2').parent().siblings().find('a').removeClass('active-2');
     });
-    //main-3
-    $('.main-3 .search .pure-form button').click(function(){
+    //main-3-search
+    $('.main-3-search .search .pure-form button').click(function(){
         $(this).toggleClass('active-3');
-        $('.main-3 .search .pure-form input').toggleClass('active-4');
+        $('.main-3-search .search .pure-form input').toggleClass('active-4');
     });
     $('.cart button').click(function(){
         $(this).toggleClass('active-5');
     });
 
-    //main-4
-    $(' .main-4 .goods button').click(function(){
+    //main-4-goods
+    $(' .main-4-goods .goods button').click(function(){
         $(this).toggleClass('active-6');
         
     });
+
+    //heart點擊後變實心
+    $('.heart a').click(function(event){
+        event.preventDefault();
+        $(this).toggleClass('active-7')
+    })
 });
