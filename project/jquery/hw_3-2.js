d6f42de41c656ce029f14b19c9baa39873a269c9
sec3582 <131690941+sec3582@users.noreply.github.com>
2023-05-31 02:29:35 +0800

    v23

--- project/jquery/hw_3-2.js
+++ project/jquery/hw_3-2.js
@@ -1,6 +1,12 @@
 $(document).ready(function(){
 
-    $('.menu li a').click(function(){
+    $('.menu li').click(function(){
         $(this).toggleClass('active-1');
+    });
+
+    //aside
+    $('.mobile-link').click(function(event){
+        $('.aside').toggleClass('open');
+        $('.dark').toggleClass('close');
     });
 });
